38fb70adbee0c66e3d0ca2212e695ff25f4e20d3
Shahali Bogdadi <bogdadishahali@gmail.com>
2019-06-19 18:05:01 +0600

    button module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatButtonToggleModule, MatBadgeModule } from "@angular/material";
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material'
+
+const MaterialComponents = [
+    MaterialModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: []
+})
+export class MaterialModule { }
